876fc0523a6643ad61de9dce37863b0f70f41fd2
taitank16 <150045263+taitank16@users.noreply.github.com>
2023-11-24 01:03:14 +0700

    Update Day9_Practice4.sql

--- Day9_Practice4.sql
+++ Day9_Practice4.sql
@@ -22,5 +22,17 @@ round(cast(sum(case
 when call_category is null then 1
 when call_category='n/a' then 1
 else 0
-end)/count(case_id)*100 as decimal),1)
+end)/count(case_id)*100 as decimal),1) as calls_percentage
 from callers
+--ex4
+select name
+from customer
+where referee_id <> 2 or referee_id is NULL
+--ex5
+select
+case when survived=1 then 'survived' else 'non-survived' end as category,
+sum(case when pclass=1 then 1 else 0 end) as first_class,
+sum(case when pclass=2 then 1 else 0 end) as second_class,
+sum(case when pclass=3 then 1 else 0 end) as third_class
+from titanic
+group by category
