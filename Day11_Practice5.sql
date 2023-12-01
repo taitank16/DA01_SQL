ddac506fb4df7fe01fdaa5a8750eb055cbc01c8d
taitank16 <150045263+taitank16@users.noreply.github.com>
2023-12-01 12:06:01 +0700

    Update Day11_Practice5.sql

--- Day11_Practice5.sql
+++ Day11_Practice5.sql
@@ -1,6 +1,14 @@
---ex1
-select COUNTRY.Continent
+--ex1 (note: floor lam tron xuong, round(,0) se lam tron len)
+select COUNTRY.Continent,
+floor(avg(City.Population))
 from CITY
 INNER JOIN COUNTRY
 on CITY.CountryCode=COUNTRY.Code
 group by COUNTRY.Continent
+--ex2
+SELECT
+round(count(distinct texts.signup_action='Confirmed')::decimal/count(texts.signup_action),2)
+from emails
+left join texts
+on emails.email_id=texts.email_id
+--ex3
